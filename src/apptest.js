24fe8a4217f3afc72f6204e1186da82c22379c3f
nnokarazu12 <nnokarazu@gmail.com>
2021-06-23 04:11:40 -0400

    c

--- src/apptest.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-axios.post('https://zh2is19bwl.execute-api.us-east-1.amazonaws.com/dev')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Button from './components/Button'
 
+const axios = require('axios');
+axios.post('https://zh2is19bwl.execute-api.us-east-1.amazonaws.com/dev')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 ReactDOM.render(
   <Button/>,
   document.getElementById('root')
