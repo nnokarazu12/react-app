00caad9545dc3889933da06599e9862162817fa6
nnokarazu12 <nnokarazu@gmail.com>
2021-06-23 02:27:29 -0400

    c

--- src/apptest.js
+++ src/apptest.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
-axios.post('https://zh2is19bwl.execute-api.us-east-1.amazonaws.com/dev')
+axios.post({
+  method: 'post',
+  url: 'https://zh2is19bwl.execute-api.us-east-1.amazonaws.com/dev',
+
+  headers: {'Content-Type': 'application/json'}
+})
   .then(function (response) {
     console.log(response);
   })
