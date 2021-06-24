d7a4ec776a288a4a990798e01bfd0d121a56ba74
nnokarazu12 <nnokarazu@gmail.com>
2021-06-23 23:23:59 -0400

    c

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,13 @@
-import call from './api/index'
-
 function Button() {
+    const axios = require('axios');
+
+    const url = "https://zh2is19bwl.execute-api.us-east-1.amazonaws.com/dev";
+
+    async function call() {
+    const response = await axios.get(url + "/v1/compare-list");
+    const data = await response.json();
+    console.log(data);
+    }
     return <button onClick={call}>Compare</button>;
 }
 
